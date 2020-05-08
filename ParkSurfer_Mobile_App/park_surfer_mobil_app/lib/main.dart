66f89431b5716e69444d6130d23d6fec2d2e0a3b
Brieden <Sven.Brieden@posteo.de>
2020-05-08 04:13:29 +0200

    Basic Structure: StatefulWidget in StatelessWidget    - Closing Sidebar is working    - Adding additional Markers is working    - TapOn Markers is working

--- ParkSurfer_Mobile_App/park_surfer_mobil_app/lib/main.dart
+++ ParkSurfer_Mobile_App/park_surfer_mobil_app/lib/main.dart
@@ -2,24 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ParkSurferHome());
 
-class MyApp extends StatelessWidget {
+class ParkSurferHome extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MapPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MapPageState();
+  }
+}
+
+class MapPageState extends State<MapPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  List<LatLng> tappedPoints = [];
+  static LatLng cartago = LatLng(9.8575, -83.921);
+  static LatLng london = LatLng(51.5, -0.09);
+  static LatLng paris = LatLng(48.8566, 2.3522);
+  static LatLng dublin = LatLng(53.3498, -6.2603);
+
   @override
   Widget build(BuildContext context) {
-    LatLng cartago = LatLng(9.8575, -83.921);
+    var markers = tappedPoints.map((latlng) {
+      return Marker(
+        width: 80.0,
+        height: 80.0,
+        point: london,
+        builder: (ctx) => Container(
+            child: GestureDetector(
+          onTap: () {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text('Tapped on blue FlutterLogo Marker'),
+            ));
+          },
+          child: FlutterLogo(),
+        )),
+      );
+    }).toList();
+
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(title: Text('ParkSurfer')),
       drawer: Drawer(
         child: ListView(
@@ -38,21 +70,20 @@ class HomePage extends StatelessWidget {
               children: <Widget>[
                 ListTile(
                   title: Text('...'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                  onTap: () {},
                 ),
                 ListTile(
                   title: Text('Costa Rica'),
                   onTap: () {
+                    _handleTap(paris);
                     Navigator.pop(context);
+
+//                        mapController.move(dublin, 5.0);
                   },
                 ),
                 ListTile(
                   title: Text('...'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                  onTap: () {},
                 ),
               ],
             ),
@@ -61,21 +92,15 @@ class HomePage extends StatelessWidget {
               children: <Widget>[
                 ListTile(
                   title: Text('...'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                  onTap: () {},
                 ),
                 ListTile(
                   title: Text('Switzerland'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                  onTap: () {},
                 ),
                 ListTile(
                   title: Text('...'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                  onTap: () {},
                 ),
               ],
             ),
@@ -88,20 +113,17 @@ class HomePage extends StatelessWidget {
           children: [
             Padding(
               padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-              child: Text('Select in the sidebar a region of interest'),
+              child: Text('Tap to add pins'),
             ),
             Flexible(
               child: FlutterMap(
-                options: MapOptions(
-                  center: cartago,
-                  zoom: 13.0,
-                ),
+                options: MapOptions(center: london, zoom: 5.0),
                 layers: [
                   TileLayerOptions(
                     urlTemplate:
-                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    subdomains: ['a', 'b', 'c'],
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   ),
+                  MarkerLayerOptions(markers: markers)
                 ],
               ),
             ),
@@ -109,5 +131,11 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _handleTap(LatLng latlng) {
+    setState(() {
+      tappedPoints.add(latlng);
+    });
   }
 }
