091d27ec047ce095f00d296102324bd480f221e3
Brieden <Sven.Brieden@posteo.de>
2020-05-08 23:46:55 +0200

    Basic Structure: MapController: adding animatedMapMove

--- ParkSurfer_Mobile_App/park_surfer_mobil_app/lib/main.dart
+++ ParkSurfer_Mobile_App/park_surfer_mobil_app/lib/main.dart
@@ -22,7 +22,7 @@ class MapPage extends StatefulWidget {
   }
 }
 
-class MapPageState extends State<MapPage> {
+class MapPageState extends State<MapPage> with TickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   List<LatLng> tappedPoints = [];
@@ -37,6 +37,40 @@ class MapPageState extends State<MapPage> {
   void initState() {
     super.initState();
     mapController = MapController();
+  }
+
+  void _animatedMapMove(LatLng destLocation, double destZoom) {
+    // Create some tweens. These serve to split up the transition from one location to another.
+    // In our case, we want to split the transition be<tween> our current map center and the destination.
+    final _latTween = Tween<double>(
+        begin: mapController.center.latitude, end: destLocation.latitude);
+    final _lngTween = Tween<double>(
+        begin: mapController.center.longitude, end: destLocation.longitude);
+    final _zoomTween = Tween<double>(begin: mapController.zoom, end: destZoom);
+
+    // Create a animation controller that has a duration and a TickerProvider.
+    var controller = AnimationController(
+        duration: const Duration(milliseconds: 5000), vsync: this); //500
+    // The animation determines what path the animation will take. You can try different Curves values, although I found
+    // fastOutSlowIn to be my favorite.
+    Animation<double> animation =
+        CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+
+    controller.addListener(() {
+      mapController.move(
+          LatLng(_latTween.evaluate(animation), _lngTween.evaluate(animation)),
+          _zoomTween.evaluate(animation));
+    });
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.dispose();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.dispose();
+      }
+    });
+
+    controller.forward();
   }
 
   @override
@@ -84,7 +118,7 @@ class MapPageState extends State<MapPage> {
                   onTap: () {
                     _handleTap(paris);
                     Navigator.pop(context);
-                    mapController.move(dublin, 5.0);
+                    _animatedMapMove(cartago, 10.0);
                   },
                 ),
                 ListTile(
